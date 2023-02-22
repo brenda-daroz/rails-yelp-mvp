8e015c4a58f55c8a570535eaa9e1a3441c05032b
Brenda Daroz <brenda.daroz@gmail.com>
2023-02-22 15:58:12 +0100

    reviews add

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # see all restaurants
+  # get "restaurants", to: "restaurants#index", as: :restaurants
+
+  # # add a restaurant
+  # get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  # post "restaurants", to: "restaurants#create"
+
+  # # see one restaurnt
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
+  #add a review
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
 end
